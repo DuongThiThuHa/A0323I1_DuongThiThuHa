21b6ef2e0f2ffad6bc5abc1cf8c06992d783b042
duongthithuha2412 <duongthithuha241202@gmail.com>
2023-08-24 20:51:37 +0700

    Push Bai Tap

--- Module 3/Bài Tập/Bài 2 - Thiết kế và tạo CSDL/mohinh.sql
+++ Module 3/Bài Tập/Bài 2 - Thiết kế và tạo CSDL/mohinh.sql
@@ -1,1 +1,65 @@
-drop database quanlydonhang;
+create database mohinh;
+use mohinh;
+
+create table mohinh.phieuxuat(
+    sopx int auto_increment primary key,
+    ngayxuat date
+);
+
+create table mohinh.vattu(
+   mavt int primary key,
+   tenvt varchar(50)
+);
+
+create table mohinh.phieunhap(
+   sopn int auto_increment primary key,
+   ngaynhap date
+);
+
+create table mohinh.vattuphieuxuat(
+    idphieuxuat int,
+    idvattu int,
+    dgxuat float,
+    slxuat int,
+	primary key(idphieuxuat,idvattu),
+    foreign key (idphieuxuat) references mohinh.phieuxuat(sopx),
+	foreign key (idvattu) references mohinh.vattu(mavt)
+);
+
+create table mohinh.vattuphieunhap(
+    idphieunhap int,
+    idvattu int,
+    dgnhap float,
+    slnhap int,
+	primary key(idphieunhap,idvattu),
+	foreign key (idphieunhap) references mohinh.phieunhap(sopn),
+    foreign key (idvattu) references mohinh.vattu(mavt)
+);
+
+create table mohinh.dondathang(
+    sodh int auto_increment primary key,
+    ngaydh date
+);
+
+create table mohinh.nhacungcap(
+    mancc  int auto_increment primary key,
+    tenncc varchar(50),
+    diachi varchar(50),
+    sdt varchar(50)
+);
+
+create table mohinh.chitietdonhang(
+    iddonhang  int,
+    idvattu int,
+    primary key (iddonhang, idvattu),
+    foreign key (iddonhang) references mohinh.dondathang(sodh),
+	foreign key (idvattu) references mohinh.vattu(mavt)
+);
+
+create table mohinh.cungcap(
+    idncc int,
+    iddonhang  int,
+    primary key (idncc, iddonhang),
+	foreign key(idncc) references mohinh.nhacungcap(mancc),
+    foreign key (iddonhang) references dondathang(sodh)
+);
--- Module 3/Bài Tập/Bài 2 - Thiết kế và tạo CSDL/quanlybanhang.sql
+++ Module 3/Bài Tập/Bài 2 - Thiết kế và tạo CSDL/quanlybanhang.sql
@@ -1,1 +1,32 @@
-drop database quanlydonhang;
+create database quanlybanhang;
+use quanlybanhang;
+
+create table quanlybanhang.customer(
+    cid int auto_increment primary key,
+    cname varchar(25) not null,
+    cage int
+);
+
+create table quanlybanhang.order(
+    oid  int auto_increment primary key,
+    idcustomer int,
+    odate  date,
+    ototalprice float,
+    foreign key (idcustomer) references quanlybanhang.customer(cid)
+);
+
+create table quanlybanhang.product(
+    pid int auto_increment primary key,
+    pname  varchar(50),
+    pprice float
+);
+
+create table quanlybanhang.orderDetail(
+    idorder int,
+    idproduct int,
+    odqty smallint,
+    primary key(idorder, idproduct),
+    foreign key (idorder)  references quanlybanhang.order(oid),
+    foreign key (idproduct) references quanlybanhang.product(pid)
+);
+
