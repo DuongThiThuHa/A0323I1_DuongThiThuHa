b5c7e07a14853bf0c5cfea7fc861d358189cb9c3
duongthithuha2412 <duongthithuha241202@gmail.com>
2023-08-27 22:28:19 +0700

    Push Bai Tap

--- Module 3/Case Study SQL/Casestudy_furama.sql
+++ Module 3/Case Study SQL/Casestudy_furama.sql
@@ -195,40 +195,4 @@ insert into hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem) values
 (2,1,2),
 (2,12,2);
 
--- 2.Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
-select * from nhan_vien
-group by ma_nhan_vien ;
--- 3.Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
-select *, year(from_days(datediff(now(),ngay_sinh))) as age
-from khach_hang
-where (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%') and (year(from_days(datediff(now(),ngay_sinh))) between 18 and 50);
 
--- 4.Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
-select kh.ma_khach_hang, ho_ten, count(1) as so_lan_dat
-from khach_hang kh
-join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
-join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
-where lk.ten_loai_khach = 'Diamond'
-group by kh.ma_khach_hang
-order by so_lan_dat asc;
-
--- 5.Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
-select kh.ma_khach_hang, ho_ten, ten_loai_khach, hd.ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, ifnull(chi_phi_thue + so_luong * gia, 0) as tongtien
-from khach_hang kh
-left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
-left join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
-left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
-left join hop_dong_chi_tiet ct on ct.ma_hop_dong = hd.ma_hop_dong
-left join dich_vu_di_kem dk on dk.ma_dich_vu_di_kem = ct.ma_dich_vu_di_kem;
-
--- 6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
-select dv.ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu
-from dich_vu dv
-join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
-join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
-where dv.ma_dich_vu not in (
-	select dv.ma_dich_vu
-    from dich_vu dv join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
-    where ngay_lam_hop_dong between '2021-01-01' and '2021-04-01'
-)
-group by dv.ma_dich_vu;
