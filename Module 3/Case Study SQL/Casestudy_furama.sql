876fe81d2390da78a4e9ebc1c3060c458032840b
duongthithuha2412 <duongthithuha241202@gmail.com>
2023-08-28 22:38:05 +0700

    Push Bài Tập

--- Module 3/Case Study SQL/Casestudy_furama.sql
+++ Module 3/Case Study SQL/Casestudy_furama.sql
@@ -195,4 +195,39 @@ insert into hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem) values
 (2,1,2),
 (2,12,2);
 
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, "T" hoặc “K” và có tối đa 15 kí tự.
+select * from nhan_vien where ho_ten like '%H' or ho_ten like'%T' or ho_ten like '&K' and length(ho_ten) <= 15;
+
+-- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select * , 2023 - year(ngay_sinh) from khach_hang where (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%') and ( 2023 - year(ngay_sinh) between 18 and 50) ;
+
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select khach_hang.ho_ten, count(hop_dong.ma_hop_dong) as so_lan_dat_phong
+from hop_dong
+join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_hop_dong
+join loai_khach on loai_khach.ma_loai_khach = hop_dong.ma_hop_dong
+where loai_khach.ten_loai_khach = 'Diamond'
+group by khach_hang.ho_ten
+order by so_lan_dat_phong asc ;
+
+-- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). 
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
+sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia ) as tong_tien
+from khach_hang 
+left join hop_dong_chi_tiet on khach_hang.ma_khach_hang = hop_dong_chi_tiet.ma_khach_hang
+left join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_khach_hang
+left join hop_dong on hop_dong.ma_hop_dong = khach_hang.ma_khach_hang
+left join dich_vu on dich_vu.ma_dich_vu = khach_hang.ma_khach_hang;
+
+SELECT kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, 
+SUM(hdct.chi_phi_thue + hdct.so_luong * dv.gia) AS tong_tien
+FROM khach_hang kh
+LEFT JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN dich_vu dv ON hdct.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
+GROUP BY kh.ma_khach_hang;
+
+
+
 
